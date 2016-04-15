8fe3b0e7995787f0a85dea061c58cd1596862eb9
dlxp5gz <dlxp5gz@VWPNPNC14966.vwpn.emea.vwg>
2016-04-15 14:43:21 +0200

    dodane kolejne funcke

---
+++ Magazyn/src/service/DeviceComboxImpl.java
@@ -0,0 +1,17 @@
+package service;
+
+import java.util.ArrayList;
+
+import model.DeviceName;
+import serviceImpl.DeviceCombox;
+
+public class DeviceComboxImpl implements DeviceCombox {
+
+	ArrayList<DeviceName> lista = new ArrayList<DeviceName>();
+
+	@Override
+	public void setComBox() {
+
+	}
+
+}
--- Magazyn/src/service/DeviceRepo.java
+++ Magazyn/src/service/DeviceRepo.java
@@ -8,4 +8,5 @@ public interface DeviceRepo {
 	 public void displayDevice();
 
 
+
 }
